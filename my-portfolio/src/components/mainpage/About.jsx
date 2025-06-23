1c0924022a7d18a9b8c2b1b6ff7474b0d6f894e6
Brian H. Castelhano <118952129+briantheengineer@users.noreply.github.com>
2025-06-22 22:41:04 -0300

    new svgs

--- my-portfolio/src/components/mainpage/About.jsx
+++ my-portfolio/src/components/mainpage/About.jsx
@@ -1,17 +1,17 @@
 import { Element } from 'react-scroll';
 import { useTranslation } from 'react-i18next';
+import Work  from '/src/assets/Work.svg'
 export default function About ( ) {
     const { t } = useTranslation();
     return (
         <Element name="about">
-        <div className="min-h-screen yellowbg px-4 py-16 md:px-16 max-w-full overflow-x-hidden flex md:flex-wrap justify-evenly bluedark">
-            <div className="w-full h-fit">
+        <div className="min-h-screen yellowbg px-4 py-16 md:px-16 max-w-full overflow-x-hidden flex flex-wrap justify-evenly bluedark">
+            <div className="w-full h-fit border">
                 <h1 data-aos="fade-left" className="text-6xl tracking-wider w-full">about.</h1>
-                <div className='border w-1/4'></div>
             </div>
-            <div data-aos="fade-right" className="w-full border ">
-                <div className='w-1/2'><img></img></div>
-                <div className='w-1/2'> <h1 className='whitespace-pre-line'>{t('about_me')}</h1></div>
+            <div data-aos="fade-right" className="w-full border flex md:flex-wrap items-center ">
+                <div className='w-1/2 h-[90%] min-h-72 min-w-64'><img className='h-full w-full' src={Work} alt="" /></div>
+                <div className='w-1/2 min-w-48'> <h1 className='whitespace-pre-line'>{t('about_me')}</h1></div>
             </div>
         </div>
         </Element>
--- my-portfolio/src/components/mainpage/Contact.jsx
+++ my-portfolio/src/components/mainpage/Contact.jsx
@@ -1,16 +1,17 @@
 import { Element } from 'react-scroll';
+import Learning from '/src/assets/Learning.svg'
 export default function Contact () {
     return (
          <Element name="contact">
         <div className=" yellowbg px-4 min-h-screen max-w-full md:px-16 overflow-x-hidden flex md:flex-wrap justify-evenly bluedark">
             <div className="w-full">
-                <h1 className="text-6xl tracking-wider w-full ">contact.</h1>
+                <h1 className="text-6xl tracking-wider w-full ">contact me.</h1>
             </div>
-            <div className="w-full">
-                <div>
-                    <img></img>
+            <div className="w-full md:p-16">
+                <div className='w-1/2'>
+                    <img src={Learning} alt="" />
                 </div>
-                <div>
+                <div className='w-1/2'>
                     <p></p>
                 </div>
             </div>
